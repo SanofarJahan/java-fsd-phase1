bb72f9148069a4c845f9c43ed314a680706d9bf5
Sanofar Jahan M <87316028+SanofarJahan@users.noreply.github.com>
2022-09-03 16:57:20 +0530

    PHASE 3 FINAL PROJECT

---
+++ OnlineQuizPortal-REST/src/main/java/com/repository/AdminRepo.java
@@ -0,0 +1,8 @@
+package com.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bean.Admin;
+
+public interface AdminRepo extends JpaRepository<Admin, Integer> {
+
+}
