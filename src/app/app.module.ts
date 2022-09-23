8ddf5ec7f32d57d088feb2d15b0deb8d0394ff8a
Sanofar Jahan M <87316028+SanofarJahan@users.noreply.github.com>
2022-09-23 21:50:13 +0530

    phase 4- SESSION 7 myapp

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { CustomPipe } from './CustomPipe';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PipesComponent,
+    CustomPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
